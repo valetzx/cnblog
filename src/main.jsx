cd651c967023e31eaafd853a28a171e3dee18a31
valetzx <45054400+valetzx@users.noreply.github.com>
2025-09-18 19:30:24 +0800

    Add microsoft

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "tbkjmrizc7");
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>,
 );
